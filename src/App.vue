0d2c58dc9b39922862e76baa1a815104e42f2c94
nguyenphuong29082004 <nguyenphuong29082004@gmail.com>
2025-04-22 15:07:46 +0700

    document week6

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <script setup>
-import Demo from './components/Baitap/demo.vue';
-import ToDo from './components/Baitap/toDo.vue';
-
+import { ref } from 'vue';
+import Count from './components/Week6/Count.vue';
 // import child from './components/Trenlop/props/child.vue';
 // import chirdent from './components/Trenlop/props/chirdent.vue';
-
+const count = ref(0)
+const handleCount = () => {
+  count.value ++
+}
+const messageHandler = (data, other) =>{
+  if (data > 10){
+    alert(`Ket qua: ${data}`)
+  }
+  if (other > 20){
+    alert(`${other}`)
+  }
+} 
 
 </script>
 
 <template>
-  <!-- <chirdent/> -->
-  <ToDo/>
-  <!-- <Demo/> -->
+  <p>Count: {{count}}</p>
+  <button @click="handleCount">Dem</button>
+  <br>
+  <Count :count="count" @alert-message="messageHandler"/>
+
 </template>
 
 <style scoped>
---
+++ src/components/Week6/Count.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineProps,defineEmits, ref, computed  } from 'vue';
+
+const props = defineProps([
+    "count","dem"
+])
+// const c = props.count
+// const c = ref(props.count * 2)
+const c = computed(()=>{
+    return props.count * 2
+})
+const d  = props.count * 5
+const emit = defineEmits()
+const handle = () => {
+    console.log(c.value)
+    emit("alertMessage",c.value,d)
+}
+
+</script>
+<template>
+    <p>Gia tri nhan doi: {{ props.count * 2 }}</p>
+    <!-- <button @click="emit("alertMessage",c)">Alert</button> -->
+     <button @click="handle">Alert</button>
+
+</template>
